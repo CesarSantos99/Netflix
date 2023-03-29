61eb91b01a0e62448f28a1a360995b1ab4db3d57
César <cesarsantossouza99@gmail.com>
2023-03-29 14:13:14 -0300

    Fim

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 let botao = document.getElementById("botao-pergunta")
 let menu = document.getElementById("pergunta")
-let imagem = document.getElementById("fechar")
+let imagem = document.getElementById("fechar-netflix")
 
 
 function abacate() {
-    if (menu.style.display == "block") {
-        menu.style.display = "none"
-        fechar.id = "fechar"
-    }else{
+    if (menu.style.display == "none") {
         menu.style.display = "block"
-        fechar.id = "fechar-girar"
+        imagem.id = "fechar-girar"
+    } else{
+        menu.style.display = "none"
+        imagem.id = "fechar-netflix"
     }
 }
 
